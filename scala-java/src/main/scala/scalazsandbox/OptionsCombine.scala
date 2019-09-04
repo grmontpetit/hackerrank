fbf3f5e9975960197cd4e65cd0f282f351edf05d
sniggel <grmontpetit@gmail.com>
2019-09-04 10:28:51 -0400

    Added monoid and Option operator.

--- scala-java/src/main/scala/scalazsandbox/OptionsCombine.scala
+++ scala-java/src/main/scala/scalazsandbox/OptionsCombine.scala
@@ -1,17 +1,30 @@
 package scalazsandbox
 
-import scalaz._
-import Scalaz._
+import scalaz.{Applicative, Monoid}
+import scalaz.Scalaz._
 
 object OptionsCombine {
 
   def main(args: Array[String]): Unit = {
     val option1 = Some(1)
-    val option2 = None
+    val option2 = none[Int]
     println(option2.orElse(option1))
     println(option1.orElse(option2))
-
     assert(option2.orElse(option1) == option1.orElse(option2))
+
+    println(option2.orElse(option1).orZero)
+
+    val x = none
+    val y = 1.some
+    println(s"result = ${x <* y}")
   }
+
+  implicit def IntMonoid: Monoid[Option[Int]] = new Monoid[Option[Int]] {
+    override def zero: Option[Int] = none
+    override def append(f1: Option[Int], f2: => Option[Int]): Option[Int] = {
+      f1.map(x => f2.fold(x){y => x + y})
+    }
+  }
+
 
 }
